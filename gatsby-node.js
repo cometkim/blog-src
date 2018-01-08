b6d99ad03ddb868fa0b00eb795613b6de8b6d2ad
Hyeseong Kim <cometkim.kr@gmail.com>
2018-01-09 02:01:39 +0900

    Add gatsby node to create page with the template

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+exports.createPages = async ({ boundActionCreators, graphql }) => {
+    const query = `{
+        posts: allMarkdownRemark {
+            edges {
+                node {
+                    html
+                    id
+                    frontmatter {
+                        path
+                        title
+                    }
+                }
+            }
+        }
+    }`;
+    const { data, errors } = await graphql(query);
+
+    if (errors) {
+        throw new Error(errors);
+    }
+
+    const { createPage } = boundActionCreators;
+
+    data.posts.edges
+        .map(p => p.node)
+        .map(post => ({
+            path: post.frontmatter.path,
+            component: `${__dirname}/src/templates/post.tsx`,
+            context: {
+                path: post.frontmatter.path
+            }
+        }))
+        .forEach(ctx => createPage(ctx));
+}
