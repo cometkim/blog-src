8e38e693d7bc9304d0839c585ad5df9031237f94
Hyeseong Kim <cometkim.kr@gmail.com>
2018-01-24 03:06:29 +0900

    Update the default layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react'
 
+import Helmet from 'react-helmet'
+
+import config from '../site-config'
 import './index.scss'
 
 interface DefaultLayoutProps {
     children: any
+    location: {
+        pathname: string
+    }
 }
 
-export default ({ children }: DefaultLayoutProps) => <div>{children()}</div>
+export default ({ children, location }: DefaultLayoutProps) => {
+    const { pathname } = location
+    const { siteUrl, title, description, author, keywords } = config
+
+    return (
+        <div>
+            <Helmet>
+                {/* 사이트 기본 메타 정보는 대부분의 페이지에서 Override 되며, 생략된 경우만 사용 */}
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="author" content={author} />
+                <meta name="keywords" content={keywords.join(', ')} />
+
+                <link rel="canonical" href={siteUrl + pathname} />
+            </Helmet>
+
+            {children()}
+        </div>
+    )
+}
