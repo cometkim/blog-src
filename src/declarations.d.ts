522d23170dc1c54227bdf37ca4a7cfd355b81194
Hyeseong Kim <cometkim.kr@gmail.com>
2018-02-07 01:37:17 +0900

    인덱스 페이지에 글목록 추가

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,23 @@
 declare const graphql: (query: TemplateStringsArray) => void
+
+interface IndexPageProps {
+    data: {
+        allMarkdownRemark: {
+            edges: Array<{
+                node: {
+                    internal: {
+                        contentDigest: string
+                    }
+                    excerpt: string
+                    frontmatter: {
+                        path: string
+                        title: string
+                        author: string
+                        date: string
+                        tags: Array<string>
+                    }
+                }
+            }>
+        }
+    } 
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,45 @@ import config from '../site-config';
 
 import Header from '../components/header';
 import ProfileCard from '../components/profile-card';
-import PostList from '../components/post-list';
+import PostCard from '../components/post-card';
 
-export default () => {
+export default ({ data }: IndexPageProps) => {
+    const postCards = data.allMarkdownRemark.edges
+        .map(edge => edge.node)
+        .map(({ excerpt, frontmatter, internal }) => ({ excerpt, ...frontmatter, ...internal }))
+        .map(props => (
+            <li key={props.contentDigest}>
+                <PostCard {...props} />
+            </li>
+        ))
+
     return [
-            <Header title={config.title} />,
-            <ProfileCard />,
-            <PostList />,
+        <Header key='header' title={config.title} />,
+        <ProfileCard key='profile-card' />,
+        <ul key='post-card-list'>{postCards}</ul>,
     ]
 }
+
+export const pageQuery = graphql`
+    query IndexQuery {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+        ) {
+            edges {
+                node {
+                    internal {
+                        contentDigest
+                    }
+                    excerpt
+                    frontmatter {
+                        path
+                        title
+                        author
+                        date
+                        tags
+                    }
+                }
+            }
+        }
+    }
+`
