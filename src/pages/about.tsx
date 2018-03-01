0c5dac766a63aa1f65a41ac8d177221bd24b30e7
Hyeseong Kim <cometkim.kr@gmail.com>
2018-03-01 21:49:06 +0900

    WIP: About 페이지 추가

---
+++ src/pages/about.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+import {
+    Header,
+    Footer,
+} from 'components'
+
+type AboutPageProps = SiteData
+
+export default ({ data }: AboutPageProps) => (
+    <>
+        <Header fixed title={data.site.siteMetadata.title} />
+        <Footer owner={data.site.siteMetadata.owner.name} />
+    </>
+)
+
+export const pageQuery = graphql`
+    query AboutQuery {
+        site {
+            siteMetadata {
+                title
+                owner {
+                    name
+                }
+            }
+        }
+    }
+`
