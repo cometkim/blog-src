840568cd304e6803252dafddb1457f80f2a1e8bc
Hyeseong Kim <cometkim.kr@gmail.com>
2018-03-02 18:27:05 +0900

    Utterance 댓글 추가 Closes #24

--- src/components/utteranc.tsx
+++ src/components/utteranc.tsx
@@ -6,13 +6,22 @@ interface UtterancProps {
     issueTerm: string
 }
 
-export default ({ repo, branch, issueTerm }: UtterancProps) => (
-    <script
-        src='https://utteranc.es/client.js'
-        // @ts-ignore
-        repo={repo}
-        branch={branch}
-        issue-term={issueTerm}
-        async
-    />
-)
+export default class Utteranc extends React.PureComponent<UtterancProps> {
+    instance: HTMLDivElement = null;
+
+    componentDidMount() {
+        const utteranc = document.createElement('script')
+        utteranc.src = 'https://utteranc.es/client.js'
+        utteranc.async = true
+
+        utteranc.setAttribute('repo', this.props.repo)
+        utteranc.setAttribute('branch', this.props.branch)
+        utteranc.setAttribute('issue-term', this.props.issueTerm)
+
+        this.instance.appendChild(utteranc)
+    }
+
+    render() {
+        return <div ref={el => (this.instance = el)} />
+    }
+}
