166483c2a072d1808e5f8e697f2bfc384fb444c3
Hyeseong Kim <cometkim.kr@gmail.com>
2018-03-25 19:55:39 +0900

    간단한 404 페이지 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
 
-const NotFoundPage = () => (
-    <div>
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+export default () => (
+    <Container>
+        <Shrug>¯\_(ツ)_/¯</Shrug>
+        <Description>404... 요청하신 페이지를 찾을 수 없습니다. </Description>
+        <Link to='/'>홈으로 돌아가기</Link>
+    </Container>    
 )
 
-export default NotFoundPage
+const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+`
+
+const Shrug = styled.h1`
+`
+
+const Description = styled.div`
+    font-size: 1.3rem;
+`
