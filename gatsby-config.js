bc5757f0bb67723784b986d0992d957cc8d5c716
Hyeseong Kim <cometkim.kr@gmail.com>
2018-04-11 03:48:17 +0900

    Sharp 플러그인 사용위치 올바르게 고침

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
         'gatsby-plugin-feed',
         'gatsby-plugin-offline',
         'gatsby-plugin-netlify',
+        'gatsby-plugin-sharp',
         {
             resolve: 'gatsby-plugin-canonical-urls',
             options: {
@@ -61,9 +62,7 @@ module.exports = {
         {
             resolve: 'gatsby-transformer-remark',
             options: {
-                plugins: [
-                    'gatsby-plugin-sharp',
-                    {
+                plugins: [{
                         resolve: 'gatsby-remark-images',
                         options: {
                             maxWidth: 590,
