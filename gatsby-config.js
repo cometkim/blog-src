95d9bfb31d7e0cf5cdcb59e17d5b071fc5f99841
Hyeseong Kim <cometkim.kr@gmail.com>
2018-05-08 00:12:46 +0900

    gatsby-node 코드 리팩터링 (#60)
    * Refactor the gatsby-node
    * gatsby-node 에서 유틸 코드 분리 (허용 안됨)
    * 시리즈 생성 규칙 테스트 추가
    * 슬러그 생성 규칙 테스트 추가
    * 페이지 생성 쿼리 스냅샷 테스트 추가
    * 페이지 리디렉션 스냅샷 테스트 추가
    * 포스트 페이지 생성자 테스트 추가
    * 시리즈 페이지 생성자 테스트 추가
    * 태그 페이지 생성자 테스트 추가
    
    Closes #56

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,8 @@ module.exports = {
         {
             resolve: 'gatsby-transformer-remark',
             options: {
-                plugins: [{
+                plugins: [
+                    {
                         resolve: 'gatsby-remark-images',
                         options: {
                             maxWidth: 590,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,112 +1,48 @@
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.onPostBuild = () => {}
+const {
+    creators,
+    redirects,
+    buildSlug,
+    buildSeries
+} = require('./node-utils')
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     if (node.internal.type === 'MarkdownRemark') {
-        const categorySlug = '/posts'
-        const postSlug = createFilePath({
-            node,
-            getNode,
-            basePath: 'blog-posts/posts',
-        })
-        const slug = categorySlug + postSlug
+        const { slug, postSlug } = buildSlug({ context: { node, getNode }, createFilePath })
         console.log(`\n- Gen Slug: ${slug}`)
-
         boundActionCreators.createNodeField({
             node,
             name: 'slug',
             value: slug,
         })
-
-        const trimSlash = slug => slug.slice(
-            1, // the slug is always began with `/`
-            slug.endsWith('/') ? -1 : 0,
-        )
-        const [series, name] = trimSlash(postSlug).split('/')
-        if (name) {
-            boundActionCreators.createNodeField({
-                node,
-                name: 'series',
-                value: series,
-            })
-        }
+        
+        const { series } = buildSeries({ postSlug })
+        console.log(`-> Series ${series}`)
+        boundActionCreators.createNodeField({
+            node,
+            name: 'series',
+            value: series,
+        })
     }
 }
 
-exports.createPages = async ({ boundActionCreators, graphql }) => {
-    const postQuery = `{
-        posts: allMarkdownRemark {
-            edges {
-                node {
-                    fields {
-                        slug
-                    }
-                }
-            }
-        }
-    }`
-    const postData = await graphql(postQuery);
-    if (postData.errors) {
-        throw new Error(errors);
-    }
-    postData.data.posts.edges
-        .map(e => e.node)
-        .map(n => n.fields.slug)
-        .map(slug => ({
-            path: slug,
-            component: `${__dirname}/src/templates/post.tsx`,
-            context: { slug },
-        }))
-        .forEach(ctx => boundActionCreators.createPage(ctx))
+exports.createPages = async ({
+    graphql,
+    boundActionCreators: {
+        createPage,
+        createRedirect,
+    },
+}) => {
+    const queries = creators.map(c => graphql(c.query))
+    const results = await Promise.all(queries)
+    creators.forEach(({ creator }, index) => {
+        const { data, errors } = results[index]
 
-    const seriesQuery = `{
-        posts: allMarkdownRemark(
-            filter: { fields: { series: { ne: null } } }
-        ){
-            group(field: fields___series){
-                series: fieldValue
-            }
+        if (!errors) {
+            creator({ data, createPage })
         }
-    }`
-    const seriesData = await graphql(seriesQuery)
-    if (seriesData.errors) {
-        throw new Error(errors)
-    }
-    seriesData.data.posts.group
-        .map(group => group.series)
-        .map(series => ({
-            path: `/series/${series}/`,
-            component: `${__dirname}/src/templates/series.tsx`,
-            context: { series },
-        }))
-        .forEach(ctx => boundActionCreators.createPage(ctx))
+    })
 
-    const tagsQuery = `{
-        posts: allMarkdownRemark(
-            filter: { frontmatter: { tags: { ne: null } } }
-        ) {
-            group(field: frontmatter___tags) {
-                tag: fieldValue
-            }
-        }
-    }
-    `
-    const tagsData = await graphql(tagsQuery)
-    if (tagsData.errors) {
-        throw new Error(tagsData.errors)
-    }
-    tagsData.data.posts.group
-        .map(group => group.tag)
-        .map(tag => ({
-            path: `/tags/${tag}/`,
-            component: `${__dirname}/src/templates/tag.tsx`,
-            context: { tag },
-        }))
-        .forEach(ctx => boundActionCreators.createPage(ctx))
-    const { createRedirect } = boundActionCreators;
-    createRedirect({
-        fromPath: '/posts/what-i-leared-from-contribution-and-rfc-1341/',
-        toPath: '/posts/what-i-learned-from-contribution-and-rfc-1341/',
-        isPermanent: true })
+    redirects.forEach(r => createRedirect(r))
 }
