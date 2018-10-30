ed1da1f8ad3373b50fc15f5761301adf3f17f84e
Hyeseong Kim <cometkim.kr@gmail.com>
2018-10-31 01:15:21 +0900

    Upgrade StyledComponents to v4

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,41 +1,11 @@
 import * as React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import { injectGlobal } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 import { SiteHelmet } from 'components'
 import theme from 'utils/theme'
 
 import 'assets/hack-subset.css'
-
-// tslint:disable-next-line:no-unused-expression
-injectGlobal`
-    @import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
-
-    body {
-        font-family: 'Noto Sans KR', sans-serif;
-        word-break: keep-all;
-        color: ${theme.blackColor};
-        -webkit-font-smoothing: antialiased;
-    }
-
-    a {
-        color: ${theme.blackColor};
-        text-decoration: none;
-        transition: color .2s ease;
-
-        :focus, :hover, :active {
-            color: ${theme.primaryColor};
-        }
-    }
-
-    pre, code {
-        font-family: 'Hack', monospace;
-    }
-
-    html, body {
-        margin: 0;
-    }
-`
 
 interface LayoutProps {
     children: any
@@ -63,6 +33,34 @@ export default ({ children }: LayoutProps) => (
             <>
                 {/* 사이트 기본 메타 정보는 대부분의 페이지에서 Override 되며, 생략된 경우만 사용 */}
                 <SiteHelmet {...siteMetadata}/>
+                {createGlobalStyle`
+                    @import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+
+                    body {
+                        font-family: 'Noto Sans KR', sans-serif;
+                        word-break: keep-all;
+                        color: ${theme.blackColor};
+                        -webkit-font-smoothing: antialiased;
+                    }
+
+                    a {
+                        color: ${theme.blackColor};
+                        text-decoration: none;
+                        transition: color .2s ease;
+
+                        :focus, :hover, :active {
+                            color: ${theme.primaryColor};
+                        }
+                    }
+
+                    pre, code {
+                        font-family: 'Hack', monospace;
+                    }
+
+                    html, body {
+                        margin: 0;
+                    }
+                `}
                 {children}
             </>
         )}
