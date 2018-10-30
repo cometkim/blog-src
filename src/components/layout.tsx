bab20bb12af0c8ba0e66ad98f189bebc9f5648d3
Hyeseong Kim <cometkim.kr@gmail.com>
2018-10-31 01:26:25 +0900

    글로벌 스타일 미적용 문제 수정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,6 +7,35 @@ import theme from 'utils/theme'
 
 import 'assets/hack-subset.css'
 
+const GlobalStyle = createGlobalStyle`
+    @import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+
+    body {
+        font-family: 'Noto Sans KR', sans-serif;
+        word-break: keep-all;
+        color: ${theme.blackColor};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    a {
+        color: ${theme.blackColor};
+        text-decoration: none;
+        transition: color .2s ease;
+
+        :focus, :hover, :active {
+            color: ${theme.primaryColor};
+        }
+    }
+
+    pre, code {
+        font-family: 'Hack', monospace;
+    }
+
+    html, body {
+        margin: 0;
+    }
+`
+
 interface LayoutProps {
     children: any
 }
@@ -33,34 +62,7 @@ export default ({ children }: LayoutProps) => (
             <>
                 {/* 사이트 기본 메타 정보는 대부분의 페이지에서 Override 되며, 생략된 경우만 사용 */}
                 <SiteHelmet {...siteMetadata}/>
-                {createGlobalStyle`
-                    @import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
-
-                    body {
-                        font-family: 'Noto Sans KR', sans-serif;
-                        word-break: keep-all;
-                        color: ${theme.blackColor};
-                        -webkit-font-smoothing: antialiased;
-                    }
-
-                    a {
-                        color: ${theme.blackColor};
-                        text-decoration: none;
-                        transition: color .2s ease;
-
-                        :focus, :hover, :active {
-                            color: ${theme.primaryColor};
-                        }
-                    }
-
-                    pre, code {
-                        font-family: 'Hack', monospace;
-                    }
-
-                    html, body {
-                        margin: 0;
-                    }
-                `}
+                <GlobalStyle/>
                 {children}
             </>
         )}
