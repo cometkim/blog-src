4ca6b0231a162941de27c30e6fe47289804c9e58
Hyeseong Kim <cometkim.kr@gmail.com>
2019-03-15 02:48:21 +0900

    Gatsby node 관련 유틸 코드 재작성

---
+++ bootstrap/filed-util.js
@@ -0,0 +1,29 @@
+const { createFilePath } = require('gatsby-source-filesystem')
+
+const buildSlug = (context) => () => {
+    const { node, getNode } = context
+
+    const basePath = 'blog-posts/posts'
+    return createFilePath({ node, getNode, basePath })
+}
+
+const buildSeries = (context) => (slug) => {
+    const trimSlash = path => path.slice(
+        1, // the slug is always began with `/`
+        slug.endsWith('/') ? -1 : 0,
+    )
+
+    const [series, name] = trimSlash(slug).split('/')
+    return name ? series : null
+}
+
+const createFieldUtil = (context) => ({
+    buildSlug: buildSlug(context),
+    buildSeries: buildSeries(context),
+})
+
+module.exports = {
+    createFieldUtil,
+    buildSlug,
+    buildSeries,
+}
