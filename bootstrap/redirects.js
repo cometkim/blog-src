fd66d3892efb7e71705fb87a11b0afb61190660c
Hyeseong Kim <cometkim.kr@gmail.com>
2019-03-15 03:39:51 +0900

    Gatsby node 관련 유틸 코드 재작성 (#81)
    * refactor: page, field 생성용 유틸코드 리팩터링
    
    * fix(bug): series 생성 규칙 버그 수정

---
+++ bootstrap/redirects.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {
+        fromPath: '/posts/what-i-leared-from-contribution-and-rfc-1341/',
+        toPath: '/posts/what-i-learned-from-contribution-and-rfc-1341/',
+        isPermanent: true,
+    },
+]
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,52 +1,130 @@
-const { createFilePath } = require('gatsby-source-filesystem')
+const { createFieldUtil } = require('./bootstrap/filed-util')
+const { createPageUtil } = require('./bootstrap/page-util')
+const redirects = require('./bootstrap/redirects')
 
-const {
-    creators,
-    redirects,
-    buildSlug,
-    buildSeries
-} = require('./node-utils')
+// Does nothing but for hinting syntax highlight of inline GraphQL query
+const graphql = lit => lit[0]
 
-exports.onCreateNode = ({
-    node,
-    getNode,
-    actions: { createNodeField },
+// Create redirections
+exports.onPostBootstrap = ({
+    actions: { createRedirect },
 }) => {
-    if (node.internal.type === 'MarkdownRemark') {
-        const { slug, postSlug } = buildSlug({ context: { node, getNode }, createFilePath })
+    redirects.forEach(redirect => createRedirect(redirect))
+}
+
+// Create custom fields on nodes
+// - Slug (equals with path of URL)
+// - Series (Split the slug by `/`, then equals with the first part if it have a couple, otherwise null)
+exports.onCreateNode = ({
+    actions: { createNodeField },
+    ...context
+}) => {
+    const util = createFieldUtil(context)
+
+    if (context.node.internal.type === 'MarkdownRemark') {
+        const prefix = '/posts'
+        const slug = prefix + util.buildSlug()
         console.log(`\n- Gen Slug: ${slug}`)
         createNodeField({
-            node,
+            node: context.node,
             name: 'slug',
             value: slug,
         })
         
-        const { series } = buildSeries({ postSlug })
+        const series = util.buildSeries(slug)
         console.log(`-> Series ${series}`)
         createNodeField({
-            node,
+            node: context.node,
             name: 'series',
             value: series,
         })
     }
 }
 
-exports.createPages = async ({
-    graphql,
-    actions: {
-        createPage,
-        createRedirect,
-    },
-}) => {
-    const queries = creators.map(c => graphql(c.query))
-    const results = await Promise.all(queries)
-    creators.forEach(({ creator }, index) => {
-        const { data, errors } = results[index]
+// Create contentful pages
+// - Post pages (`/posts/{slug}`)
+// - Series pages (`/series/{series}`)
+// - Tag pages (`/tag/{tag}`)
+exports.createPages = async (context) => {
+    const util = createPageUtil(context)
 
-        if (!errors) {
-            creator({ data, createPage })
-        }
+    const postPageCreator = util.createPageCreator({
+        query: graphql`{
+            posts: allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }`,
+        mapDataToProps: data => (
+            data.posts.edges
+            .map(edge => edge.node)
+            .map(node => node.fields.slug)
+            .map(slug => ({
+                path: slug,
+                component: `${__dirname}/src/templates/post.tsx`,
+                context: { slug },
+            }))
+        ),
     })
 
-    redirects.forEach(r => createRedirect(r))
+    const seriesPageCreator = util.createPageCreator({
+        query: graphql`{
+            posts: allMarkdownRemark(
+                filter: {
+                    fields: {
+                        series: { ne: null }
+                    }
+                }
+            ) {
+                groups: group(field: fields___series){
+                    series: fieldValue
+                }
+            }
+        }`,
+        mapDataToProps: data => (
+            data.posts.groups
+            .map(group => group.series)
+            .map(series => ({
+                path: `/series/${series}`,
+                component: `${__dirname}/src/templates/series.tsx`,
+                context: { series },
+            })
+        )),
+    })
+
+    const tagPageCreator = util.createPageCreator({
+        query: graphql`{
+            posts: allMarkdownRemark(
+                filter: {
+                    frontmatter: {
+                        tags: { ne: null }
+                    }
+                }
+            ) {
+                groups: group(field: frontmatter___tags) {
+                    tag: fieldValue
+                }
+            }
+        }`,
+        mapDataToProps: data => (
+            data.posts.groups
+            .map(group => group.tag)
+            .map(tag => ({
+                path: `/tags/${tag}`,
+                component: `${__dirname}/src/templates/tag.tsx`,
+                context: { tag },
+            }))
+        ),
+    })
+
+    await Promise.all([
+        postPageCreator(),
+        seriesPageCreator(),
+        tagPageCreator(),
+    ])
 }
